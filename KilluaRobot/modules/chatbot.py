0484ff022ff517b4306dc2f58688a4df5bdc3611
testosmani <101720965+testosmani@users.noreply.github.com>
2022-04-12 04:51:36 -0700

    Update chatbot.py

--- KilluaRobot/modules/chatbot.py
+++ KilluaRobot/modules/chatbot.py
@@ -160,12 +160,12 @@ def list_all_chats(update: Update, context: CallbackContext):
 
 
 __help__ = """
-Chatbot utilizes the Kuki's api which allows Kuki to talk and provide a more interactive group chat experience.
+Chatbot utilizes the Osmani's api which allows Kuki to talk and provide a more interactive group chat experience.
 
 *Admins only Commands*:
 ➢ `/Chatbot`*:* Shows chatbot control panel
   
-*Powered by ItelAi*
+*Powered by @ribajosmani*
 """
 
 __mod_name__ = "ChatBot"
