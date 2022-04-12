997ebd0dd6af7341d037484bbb27a881f415dc6c
testosmani <101720965+testosmani@users.noreply.github.com>
2022-04-12 04:50:14 -0700

    Update alive.py

--- KilluaRobot/modules/alive.py
+++ KilluaRobot/modules/alive.py
@@ -9,22 +9,22 @@ from KilluaRobot.events import register
 from KilluaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/e96eb51fbaf1123d7e27b.jpg"
+PHOTO = "https://telegra.ph/file/02e51c229a2867e13c21e.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hello [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Killua Robot** \n\n"
+    TEXT = f"**Hello [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Osmani Robot** \n\n"
     TEXT += "✥ **I'm Working Properly** \n\n"
-    TEXT += f"✥ **My Master : [Apis](https://t.me/tzypis)** \n\n"
+    TEXT += f"✥ **My Master : [Ribaj Osmani](https://t.me/ribajosmani)** \n\n"
     TEXT += f"✥ **Library Version :** `{telever}` \n\n"
     TEXT += f"✥ **Telethon Version :** `{tlhver}` \n\n"
     TEXT += f"✥ **Pyrogram Version :** `{pyrover}` \n\n"
     TEXT += "**Thanks For Adding Me Here 😼**"
     BUTTON = [
         [
-            Button.url("Help", "https://t.me/TheKilluaRobot?start=help"),
-            Button.url("Support", "https://t.me/Killua_Support"),
+            Button.url("Help", "https://t.me/RibLentBot?start=help"),
+            Button.url("Support", "https://t.me/osmanigroupbot"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
